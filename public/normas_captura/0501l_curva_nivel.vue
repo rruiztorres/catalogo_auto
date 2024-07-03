d3ff5f0c17ac4b9218aff218c90e6914211b6366
Raúl Ruiz Torres <rrtorres@transportes.gob.es>
2024-07-03 10:10:41 +0200

    Update 0501l_curva_nivel
    Actualización normas de captura de 0501 curva de nivel para igualarlas con las especificaciones de BTN.

--- public/normas_captura/0501l_curva_nivel.vue
+++ public/normas_captura/0501l_curva_nivel.vue
@@ -1,31 +1,43 @@
 <template>
 	<div>
 		<h2>Normas de captura</h2>
-<p>Como norma general se representan a través de <strong>elementos lineales continuos, sin cortes ni intersecciones entre ellas</strong> salvo en el caso de <strong>curvas auxiliares</strong> que sí pueden quedar cortadas.</p>
+		<p>Como norma general se representan a través de <strong>elementos lineales continuos, sin cortes ni intersecciones entre ellas</strong> 
+		salvo en el caso de <strong>curvas auxiliares</strong> que sí pueden quedar cortadas.</p>
 
-<p>Las curvas de nivel deberán ser <strong>consecutivas a la equidistancia debida</strong>, no debiendo existir discrepancias superiores o inferiores a los <strong>10 m.</strong> entre dos curvas de nivel consecutivas.</p>
+		<p>Las curvas de nivel deberán ser <strong>consecutivas a la equidistancia debida</strong>, 
+		no debiendo existir discrepancias superiores o inferiores a los <strong>10 m.</strong> entre dos curvas de nivel consecutivas.</p>
 
-<h2>Criterios de Selección</h2>
-<ul>
-	<li>El área mínima que delimita una curva de nivel debe ser de <strong>25x25 m2.</strong></li>
-	<li><strong>En zona de pendiente acusada o fuerte pendiente</strong> <i>(pendiente superior al 6 %)</i>, si el área es menor al área mínima, su representación se realiza a través de un punto acotado.</li>
-	<li><strong>En zona costera o con pendiente suave o muy llana</strong> <i>(pendiente inferior al 6%)</i>, si el área es menor que el área mínima representable, puede plantarse exagerar el área encerrada por la curva de nivel hasta que alcance el área mínima.</li>
-	<li>Las curvas auxiliares sólo están permitidas en Zona muy llana y con pocas irregularidades <i><strong>&lt; 1 %.</strong></i></li>
-	<li>No se representan <strong>curvas batimétricas</strong> en el interior de las zonas emergidas.</li>
-</ul>
+		<p>En la actualización de una zona ya restituida mediante el uso de ortofotografías, 
+		la altimétrica de las zonas afectadas se calcula utilizando el modelo digital de elevaciones asociado a las mismas.</p> 
+
+		<p>No se representan curvas batimétricas en el interior de las zonas emergidas.</p>
 
 
-<h3>Ejemplos de captura</h3>
-<div class="picture">
-	<i>
-		<img src="../img/0501l_curva_de_nivel_1.png">
-		<p>Detalle de curvas de nivel cerca de un embalse.</p>
-	</i>		
+		<h2>Criterios de Selección</h2>
+		<ul>
+			<li>El área mínima que delimita una curva de nivel debe ser de <strong>25x25 m2.</strong></li>
+			<li><strong>En zona de pendiente acusada o fuerte pendiente</strong> <i>(pendiente superior al 6 %)</i>, 
+			si el área es menor al área mínima, su representación se realiza a través de un punto acotado.</li>
+			<li><strong>En zona costera o con pendiente suave o muy llana</strong> <i>(pendiente inferior al 6%)</i>, 
+			si el área es menor que el área mínima representable, puede plantarse exagerar el área encerrada por la curva de nivel hasta que alcance el área mínima.</li>
+			<li>Las curvas auxiliares sólo están permitidas en Zona muy llana y con pocas irregularidades <i><strong>&lt; 1 %.</strong></i></li>
+			<li>No se representan <strong>curvas batimétricas</strong> en el interior de las zonas emergidas.</li>
+		</ul>
 
-</div>
-<h2>Notas</h2>
-<p>Las <strong>curvas de nivel sobre superficie terrestre</strong> se obtienen a partir de <strong>restitución fotogramétrica</strong>. Si la actualización planimétrica de una zona ya restituida se ha hecho mediante el <strong>uso de ortofotografías</strong> digitales, la actualización altimétrica de las zonas afectadas se calcula utilizando el modelo digital de elevaciones asociado a las mismas.</p>
 
-<p>Las <strong>curvas batimétricas se obtienen a partir de las cartas náuticas</strong> que suministra el Instituto Hidrográfico de la Marina y representan un conjunto de profundidades no equidistantes entre sí, aportando menor información batimétrica a medida que aumenta la profundidad. Tienen su propia clasificación. </p>
+		<h3>Ejemplos de captura</h3>
+		<div class="picture">
+			<i>
+				<img src="../img/0501l_curva_de_nivel_1.png">
+				<p>Detalle de curvas de nivel cerca de un embalse.</p>
+			</i>		
+
+		</div>
+		<h2>Notas</h2>
+		<p>Las curvas de nivel se obtienen a partir de <b>restitución fotogramétrica.</b></p>
+
+		<p>Las curvas <b>batimétricas</b> representan un conjunto de profundidades no equidistantes entre sí, 
+		aportando menor información batimétrica a medida que aumenta la profundidad.</p> 
+			
 	</div>
 </template>
